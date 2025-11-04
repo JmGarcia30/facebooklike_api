64c5605fe61f5e09ae2b6e5a83c5804fd32cedd7
Jmgarcia30 <jmjgarcia.student@ua.edu.ph>
2025-11-04 10:40:32 +0800

    First Commit

---
+++ src/main/java/com/garcia/facebooklike/FacebooklikeApplication.java
@@ -0,0 +1,11 @@
+package com.garcia.facebooklike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebooklikeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FacebooklikeApplication.class, args);
+    }
+}
---
+++ src/main/java/com/garcia/facebooklike/PostRepository.java
@@ -0,0 +1,6 @@
+package com.garcia.facebooklike;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
